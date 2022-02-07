75b48ca80b9f2b1b90589458e6e9b2690f3a58a5
mononokehime14 <46858121+mononokehime14@users.noreply.github.com>
2022-02-07 12:06:50 +0800

    Initialize VIT

---
+++ data_loader/datasets_importer/__init__.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+from .cuhk03 import CUHK03
+from .market1501 import Market1501
+from .dukemtmc import DukeMTMC
+from .msmt17 import MSMT17
+from .ImageDataset import ImageDataset
+
+__factory = {
+    'CUHK03': CUHK03,
+    'Market-1501': Market1501,
+    'DukeMTMC-reID': DukeMTMC,
+    'MSMT17_V2': MSMT17,
+}
+
+
+def get_names():
+    return __factory.keys()
+
+
+def init_dataset(cfg,dataset_name, *args, **kwargs):
+    if cfg.DATASETS.NAMES not in __factory.keys():
+        raise KeyError("Unknown datasets: {}".format(dataset_name))
+    return __factory[dataset_name](cfg,*args, **kwargs)
---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+from .ResNet50 import ResNet50
+from .vit_small import VIT_small
